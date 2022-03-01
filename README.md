40d01db96eb3182423df4b5a7e9e897b9c2e906e
PIYUSH PANT <piyushpant15@gmail.com>
2022-03-01 13:58:13 +0530

    done

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
-# AUTHENTICATION AND AUTHORIZATION	:lock_with_ink_pen: :key:
+# AUTHENTICATION AND AUTHORIZATION :lock_with_ink_pen: :key:
 
 ### Auth using PassportJS
 
 The PassportJS NPM package is used to add auth to our webapp. You can understand its working on its official website [PassportJS](https://www.passportjs.org/)
+
+In this module, i will be using passport local so let's download its packages
+
+- `npm install passport`
+- npm install
 
 > Piyush Pant
--- app.js
+++ app.js
@@ -1,1 +1,84 @@
-// passport
+if (process.env.NODE_ENV === 'development') {   // if in development mode then require dotenv
+    require('dotenv').config();
+}
+
+const express = require('express');
+const app = express();
+
+const mongoose = require('mongoose');
+
+const ejsMate = require('ejs-mate');
+
+const path = require('path');
+
+const db = process.env.DB || 'mongodb://localhost:27017/authPassport'
+mongoose.connect(db)
+const connection = mongoose.connection
+connection.on('error', () => {
+    console.log('Sorry cannot connect to database :-(');
+})
+connection.once('open', () => {
+    console.log('Connected to database ');
+});
+
+// secret 
+const secret = process.env.SECRET;
+
+// SETS and USE
+
+// to parse the req body
+app.use(express.urlencoded({ extended: true }));
+
+// setting views folder to serve files from its absolute path
+app.set('views', path.join(__dirname, 'views'));
+
+// view engine to be ejs
+app.set('view engine', 'ejs');
+
+// using ejs-mate to create boilerplate
+app.engine('ejs', ejsMate);
+
+
+// relative - app.use(express.static('public'));
+// absolute
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+
+// ROUTES
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+// index page - only authenticated can access
+app.get('/index', (req, res) => {
+    res.render('index');
+});
+
+// login 
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+app.post('/login', (req, res) => {
+    res.send(req.body);
+});
+
+// register
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+app.post('/register', (req, res) => {
+    res.send(req.body)
+});
+
+
+// error page not found
+app.all('*', (req, res) => {
+    res.render('error');
+});
+
+
+// listening at port 
+PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log('Listening...')
+});
